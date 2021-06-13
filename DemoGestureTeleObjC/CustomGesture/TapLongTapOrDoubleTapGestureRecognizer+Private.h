4ad813027ada51bfcdcd30820dddf24c587a1120
Đỗ Lê Duy <duydl@vng.com.vn>
2021-06-13 10:55:59 +0700

    First commit

---
+++ DemoGestureTeleObjC/CustomGesture/TapLongTapOrDoubleTapGestureRecognizer+Private.h
@@ -0,0 +1,18 @@
+//
+//  TapLongTapOrDoubleTapGestureRecognizer+Private.h
+//  DemoGestureTeleObjC
+//
+//  Created by Do Le Duy on 12/29/20.
+//
+
+#ifndef TapLongTapOrDoubleTapGestureRecognizer_Private_h
+#define TapLongTapOrDoubleTapGestureRecognizer_Private_h
+
+@interface TapLongTapOrDoubleTapGestureRecognizer(Private)
+
+- (void)_didLongTap;
+- (void)_didSingleTap;
+
+@end
+
+#endif /* TapLongTapOrDoubleTapGestureRecognizer_Private_h */
---
+++ DemoGestureTeleObjC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoGestureTeleObjC
+//
+//  Created by Do Le Duy on 12/28/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
